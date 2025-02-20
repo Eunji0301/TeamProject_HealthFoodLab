ca25ef3cc723d160f226539386b6083139c167cf
marine <kakasska1212@gmail.com>
2025-02-20 13:30:58 +0900

    내 ìmy 식사일지 수정삭제, 달력 수정

---
+++ src/main/java/com/healthFood/lab/spring2502_heathFood/repository/MyMealLogRepository.java
@@ -0,0 +1,25 @@
+package com.healthFood.lab.spring2502_heathFood.repository;
+
+import com.healthFood.lab.spring2502_heathFood.vo.FoodTracker;
+import com.healthFood.lab.spring2502_heathFood.vo.User;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+@Mapper
+public interface MyMealLogRepository {
+
+    public void savefoodTracker(String ftMealTime, String ftWriteDate, String ftFoodName,
+                                String ftFoodQuantity, String ftFoodPortion, String ftCalorie, String ftCarb,
+                                String ftProtein, String ftFat, String ftSugar, String ftSodium, int uIdx);
+
+    @Select("SELECT * FROM food_tracker " +
+            "WHERE uIdx = #{uIdx} " +
+            "AND ftWriteDate = #{ftWriteDate} " +
+            "AND ftMealTime LIKE CONCAT('%', #{ftMealTime}, '%')")
+    public List<FoodTracker> getFoodTrackerList(int uIdx, String ftWriteDate,String ftMealTime );
+}
+
+
